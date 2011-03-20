46b669de2a9cec25fed9bcd6683c02e73cdad782
OneSadCookie <onesadcookie@gmail.com>
2011-03-19 18:35:25 -0700

    use getoptlong to process arguments exec the run command so the exit status gets passed along use clang when building for 10.7 allow specifying a min OS version, rather than having choosesdk calculate it

--- choosesdk.rb
+++ choosesdk.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/ruby
 
+require 'getoptlong'
+
 NONSTANDARD_SDKS = {
     '10.3.9' => '10.3',
     '10.4u' => '10.4'
@@ -24,6 +26,7 @@ MINIMUM_DARWIN_VERSION_BY_ARCH = {
 MAXIMUM_DARWIN_VERSION_BY_ARCH = {
     'ppc' => 9,
     'ppc64' => 9,
+    'i386' => 10,
 }
 
 def current_darwin_version()
@@ -58,6 +61,8 @@ COMPILERS_BY_DARWIN_VERSION = {
     8 => ['gcc-4.0', 'g++-4.0'],
     9 => ['gcc-4.2', 'g++-4.2'],
    10 => ['gcc-4.2', 'g++-4.2'],
+   11 => ['clang',   'clang++'], # technically [llvm-]gcc-4.2 is the system compiler
+                                 # but clang is preferred for new projects
 }
 
 def cc_from_darwin_version(version)
@@ -68,10 +73,20 @@ def cxx_from_darwin_version(version)
     COMPILERS_BY_DARWIN_VERSION[version][1] || 'c++'
 end
 
-def choose_sdk(arch)
+def choose_sdk(arch, min_os)
+    if min_os then
+        min_darwin_version = macosx_to_darwin_version(min_os)
+    end
     sdks = available_sdks()
     versions = possible_darwin_versions(arch)
     versions_with_sdks = versions.find_all { |v| sdks[v] }
+    if min_os then
+        versions_with_sdks = versions_with_sdks.reject { |v| v < min_darwin_version }
+    end
+    if versions_with_sdks.empty? then
+        $stderr.puts "No SDK found"
+        exit(1)
+    end
     version = versions_with_sdks[0]
     
     {
@@ -86,28 +101,72 @@ def main
     $developer = `xcode-select -print-path`.chomp
     if $?.exitstatus != 0 then
         $stderr.puts "Can't run xcode-select -print-path"
-        exit
-    end
-
-    arch = ARGV[0]
-    if !MINIMUM_DARWIN_VERSION_BY_ARCH[arch] then
-        $stderr.puts "Unknown architecture: #{arch}"
-        exit
+        exit(1)
     end
     
-    env = choose_sdk(arch)
+    opts = GetoptLong.new(
+        # general options
+        [ '--help', '-h', GetoptLong::NO_ARGUMENT],
+
+        # options affecting the logic
+        [ '--arch',       GetoptLong::REQUIRED_ARGUMENT],
+        [ '--min-os',     GetoptLong::REQUIRED_ARGUMENT],
+
+        # options affecting the output
+        [ '--json',       GetoptLong::NO_ARGUMENT],
+        [ '--print-env',  GetoptLong::NO_ARGUMENT],
+        [ '--run',        GetoptLong::REQUIRED_ARGUMENT])
+    
+    arch = nil
+    min_os = nil
+    output = :print_env
+    run_command = nil
+    
+    opts.each do |opt, arg|
+        case opt
+        when '--help'
+            puts <<EOH
+Usage:
+    choosesdk --help
+    choosesdk --arch=<arch> [--min-os=<10.6>] [--json|--print-env|--run=<cmd>]
+EOH
+        when '--arch'
+            arch = arg
+        when '--min-os'
+            min_os = arg
+        when '--json'
+            output = :json
+        when '--print-env'
+            output = :print_env
+        when '--run'
+            output = :run
+            run_command = arg
+        end
+    end
+
+    if !arch then
+        $stderr.puts "Must specify architecture"
+        exit(1)
+    end
+    
+    if !MINIMUM_DARWIN_VERSION_BY_ARCH[arch] then
+        $stderr.puts "Unknown architecture: #{arch}"
+        exit(1)
+    end
+    
+    env = choose_sdk(arch, min_os)
     # TODO getopt_long
-    if ARGV.include?('--json') then
+    if output == :json then
         puts '{'
         puts env.map { |k, v| "    \"#{k}\": \"#{v}\""}.join(",\n")
         puts '}'
-    elsif ARGV.include?('--print-env') then
+    elsif output == :print_env then
         puts env.map { |k, v| "#{k} = #{v}"}.join("\n")
-    elsif ARGV.include?('--run') then
+    elsif output == :run then
         env.each do |k, v|
             ENV[k] = v
         end
-        system(ARGV[ARGV.index('--run') + 1])
+        exec(run_command)
     end
 end
 
